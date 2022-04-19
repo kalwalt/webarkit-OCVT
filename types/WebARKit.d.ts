db1df54874056f30c598bbad2b23c1b0d3c2e915
kalwalt <github@kalwaltart.it>
2022-04-20 00:47:20 +0200

    added LLVM_FLAGS

---
+++ types/WebARKit.d.ts
@@ -0,0 +1,107 @@
+declare global {
+    namespace NodeJS {
+        interface Global {
+            webarkit: any;
+        }
+    }
+    interface Window {
+        webarkit: any;
+    }
+}
+interface ImageObj {
+    videoWidth: number;
+    width: number;
+    videoHeight: number;
+    height: number;
+    data: Uint8ClampedArray;
+}
+interface ITrackableObj {
+    width: number;
+    height: number;
+    scale: number;
+    trackableType: string;
+    barcodeId: number;
+    url: string;
+}
+export interface WebARKitPipeline {
+    trackableLoaded?: (trackableId: number) => void;
+    trackablesLoaded?: (trackableIds: number[]) => void;
+    initialized: (cameraMatrix: number[]) => void;
+    tracking: (world: any, trackableId: number) => void;
+    trackingLost: () => void;
+    process: () => Promise<ImageObj>;
+}
+export default class WebARKit {
+    private id;
+    private width;
+    private height;
+    instance: any;
+    webarkit: any;
+    private pipeline;
+    private cameraCount;
+    private cameraParam;
+    private cameraParaFileURL;
+    private _projectionMatPtr;
+    private cameraId;
+    private cameraLoaded;
+    private camera_mat;
+    private defaultMarkerWidth;
+    private default2dHeight;
+    private framepointer;
+    private framesize;
+    private dataHeap;
+    private videoLuma;
+    private videoLumaPointer;
+    private has2DTrackable;
+    private image;
+    private listeners;
+    private _marker_count;
+    private _patternDetection;
+    private userSetPatternDetection;
+    private trackables;
+    private transform_mat;
+    private _transMatPtr;
+    private marker_transform_mat;
+    private transformGL_RH;
+    private version;
+    videoWidth: number;
+    videoHeight: number;
+    videoSize: number;
+    constructor(pipeline: WebARKitPipeline);
+    setCameraURL: (url: string) => this;
+    setVideoSize: (videoWidth: number, videoHeight: number) => this;
+    static init(pipeline: WebARKitPipeline): Promise<WebARKit>;
+    private _initialize;
+    start(): Promise<void>;
+    dispose(): void;
+    private _decorate;
+    private _converter;
+    setPatternDetectionMode(mode: number): any;
+    private _updateDetectionMode;
+    private _setPatternDetectionMode;
+    process: (image: ImageObj) => Promise<void>;
+    _processImage(image: ImageObj): void;
+    private _prepareImage;
+    getCameraProjMatrix(nearPlane?: number, farPlane?: number): Float32Array;
+    getTransformationMatrix(): Float64Array;
+    getCameraMatrix(): Float32Array;
+    loadCameraParam: (urlOrData: any) => Promise<string | Uint8Array>;
+    loadCameraParam2(urlOrData: any): Promise<string>;
+    _ajax(url: string, target: string, that: any): Promise<unknown>;
+    addTrackables(trackableObj: ITrackableObj): Promise<any>;
+    addEventListener(name: string, callback: object): void;
+    removeEventListener(name: string, callback: object): void;
+    dispatchEvent(event: {
+        name: string;
+        target: any;
+        data?: object;
+    }): void;
+    arglCameraViewRHf(glMatrix: Float32Array, glRhMatrix?: Float32Array, scale?: number): Float32Array;
+    SetLogLevel: (mode: boolean) => any;
+    GetLogLevel(): any;
+    private _loadTrackable;
+    private _loadTrackableNFT;
+    private _storeDataFile;
+    private _queryTrackableVisibility;
+}
+export {};
