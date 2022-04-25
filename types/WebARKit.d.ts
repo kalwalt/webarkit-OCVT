f3d3924101cbf769d0248a2fedaa1d70780d6f81
kalwalt <github@kalwaltart.it>
2022-04-25 18:46:31 +0200

    new libs with OrbFeatureDetector

---
+++ types/WebARKit.d.ts
@@ -0,0 +1,79 @@
+declare global {
+    namespace NodeJS {
+        interface Global {
+            webarkit: any;
+        }
+    }
+    interface Window {
+        webarkit: any;
+    }
+}
+export default class WebARKit {
+    instance: any;
+    private cameraCount;
+    private version;
+    initialiseAR: () => number;
+    isInitialized: () => boolean;
+    getARToolKitVersion: () => number;
+    arwStartRunningJS: (arCameraURL: string, width: number, height: number) => number;
+    pushVideoInit: (n: number, width: number, height: number, pixelformat: string, a: number, b: number) => number;
+    _arwUpdateAR: () => number;
+    _malloc: (numBytes: number) => number;
+    _free: (pointer: number) => void;
+    _arwGetProjectionMatrix: (nearPlane: number, farPlane: number, pointer: number) => Float32Array;
+    videoMalloc: {
+        framepointer: number;
+        framesize: number;
+        videoLumaPointer: number;
+        lumaFramePointer: number;
+        newFrameBoolPtr: number;
+        fillFlagIntPtr: number;
+        timeSecPtr: number;
+        timeMilliSecPtr: number;
+    };
+    _arwQueryTrackableVisibilityAndTransformation: (id: number, pointer: number) => Float32Array;
+    _arwCapture: () => number;
+    setValue: (pointer: number, a: number, type: string) => void;
+    stopRunning: () => void;
+    shutdownAR: () => void;
+    setLogLevel: (mode: boolean) => number;
+    getLogLevel: () => number;
+    addTrackable: (config: string) => number;
+    setTrackerOptionInt: (value: number, mode: number) => number;
+    getTrackerOptionInt: (value: number) => number;
+    setTrackerOptionFloat: (value: number, mode: number) => void;
+    getTrackerOptionFloat: (value: number) => number;
+    TrackableOptions: {
+        ARW_TRACKER_OPTION_SQUARE_PATTERN_DETECTION_MODE: {
+            value: number;
+        };
+        ARW_TRACKER_OPTION_SQUARE_THRESHOLD: {
+            value: number;
+        };
+        ARW_TRACKER_OPTION_SQUARE_THRESHOLD_MODE: {
+            value: number;
+        };
+        ARW_TRACKER_OPTION_SQUARE_MATRIX_CODE_TYPE: {
+            value: number;
+        };
+        ARW_TRACKER_OPTION_SQUARE_LABELING_MODE: {
+            value: number;
+        };
+        ARW_TRACKER_OPTION_SQUARE_BORDER_SIZE: {
+            value: number;
+        };
+        ARW_TRACKER_OPTION_SQUARE_IMAGE_PROC_MODE: {
+            value: number;
+        };
+    };
+    AR_TEMPLATE_MATCHING_COLOR_AND_MATRIX: number;
+    AR_MATRIX_CODE_DETECTION: number;
+    AR_TEMPLATE_MATCHING_COLOR: number;
+    constructor();
+    init(): Promise<this>;
+    private _decorate;
+    private converter;
+    loadCameraParam(urlOrData: any): Promise<string>;
+    _ajax(url: string, target: string, that: any): Promise<unknown>;
+    private _storeDataFile;
+}
