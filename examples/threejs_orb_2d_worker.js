71525b36a88132f54e422ca0a09fcc383d144900
kalwalt <info@kalwaltart.it>
2022-05-02 16:13:13 +0200

    improved orb_2d example

--- examples/threejs_orb_2d_worker.js
+++ examples/threejs_orb_2d_worker.js
@@ -49,7 +49,7 @@ function start(markerUrl, video, input_width, input_height, render_update, track
   sphere.position.z = 0;
   sphere.position.x = 100;
   sphere.position.y = 100;
-  sphere.scale.set(200, 200, 200);
+  sphere.scale.set(100, 100, 100);
 
   root.matrixAutoUpdate = false;
   root.add(sphere);
